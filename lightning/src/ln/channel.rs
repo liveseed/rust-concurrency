lightningdevkit/rust-lightning

commit 60b962a18ebcf494340ddc001870f8160c625968

Move ChannelManager to Channel's new block data API
This also moves the scanning of the block for commitment
transactions into channel, unifying the error path.

lightning/src/ln/channel.rs
@@ -7,7 +7,6 @@
 // You may not use this file except in accordance with one or both of these
 // licenses.
 
-use bitcoin::blockdata::block::BlockHeader;
 use bitcoin::blockdata::script::{Script,Builder};
 use bitcoin::blockdata::transaction::{TxIn, TxOut, Transaction, SigHashType};
 use bitcoin::blockdata::opcodes;
@@ -3502,47 +3501,63 @@ impl<Signer: Sign> Channel<Signer> {
 		self.network_sync == UpdateStatus::DisabledMarked
 	}
 
-	pub fn transactions_confirmed(&mut self, block_hash: &BlockHash, height: u32, txdata: &TransactionData) -> Result<(), msgs::ErrorMessage> {
+	/// When a transaction is confirmed, we check whether it is or spends the funding transaction
+	/// In the first case, we store the confirmation height and calculating the short channel id.
+	/// In the second, we simply return an Err indicating we need to be force-closed now.
+	pub fn transactions_confirmed<L: Deref>(&mut self, block_hash: &BlockHash, height: u32, txdata: &TransactionData, logger: &L)
+			-> Result<(), msgs::ErrorMessage> where L::Target: Logger {
 		let non_shutdown_state = self.channel_state & (!MULTI_STATE_FLAGS);
-		if non_shutdown_state & !(ChannelState::TheirFundingLocked as u32) == ChannelState::FundingSent as u32 {
-			for &(index_in_block, tx) in txdata.iter() {
-				let funding_txo = self.get_funding_txo().unwrap();
-				if tx.txid() == funding_txo.txid {
-					let txo_idx = funding_txo.index as usize;
-					if txo_idx >= tx.output.len() || tx.output[txo_idx].script_pubkey != self.get_funding_redeemscript().to_v0_p2wsh() ||
-							tx.output[txo_idx].value != self.channel_value_satoshis {
-						if self.is_outbound() {
-							// If we generated the funding transaction and it doesn't match what it
-							// should, the client is really broken and we should just panic and
-							// tell them off. That said, because hash collisions happen with high
-							// probability in fuzztarget mode, if we're fuzzing we just close the
-							// channel and move on.
-							#[cfg(not(feature = "fuzztarget"))]
-							panic!("Client called ChannelManager::funding_transaction_generated with bogus transaction!");
-						}
-						self.channel_state = ChannelState::ShutdownComplete as u32;
-						self.update_time_counter += 1;
-						return Err(msgs::ErrorMessage {
-							channel_id: self.channel_id(),
-							data: "funding tx had wrong script/value".to_owned()
-						});
-					} else {
-						if self.is_outbound() {
-							for input in tx.input.iter() {
-								if input.witness.is_empty() {
-									// We generated a malleable funding transaction, implying we've
-									// just exposed ourselves to funds loss to our counterparty.
-									#[cfg(not(feature = "fuzztarget"))]
-									panic!("Client called ChannelManager::funding_transaction_generated with bogus transaction!");
+		for &(index_in_block, tx) in txdata.iter() {
+			if let Some(funding_txo) = self.get_funding_txo() {
+				// If we haven't yet sent a funding_locked, but are in FundingSent (ignoring
+				// whether they've sent a funding_locked or not), check if we should send one.
+				if non_shutdown_state & !(ChannelState::TheirFundingLocked as u32) == ChannelState::FundingSent as u32 {
+					if tx.txid() == funding_txo.txid {
+						let txo_idx = funding_txo.index as usize;
+						if txo_idx >= tx.output.len() || tx.output[txo_idx].script_pubkey != self.get_funding_redeemscript().to_v0_p2wsh() ||
+								tx.output[txo_idx].value != self.channel_value_satoshis {
+							if self.is_outbound() {
+								// If we generated the funding transaction and it doesn't match what it
+								// should, the client is really broken and we should just panic and
+								// tell them off. That said, because hash collisions happen with high
+								// probability in fuzztarget mode, if we're fuzzing we just close the
+								// channel and move on.
+								#[cfg(not(feature = "fuzztarget"))]
+								panic!("Client called ChannelManager::funding_transaction_generated with bogus transaction!");
+							}
+							self.channel_state = ChannelState::ShutdownComplete as u32;
+							self.update_time_counter += 1;
+							return Err(msgs::ErrorMessage {
+								channel_id: self.channel_id(),
+								data: "funding tx had wrong script/value or output index".to_owned()
+							});
+						} else {
+							if self.is_outbound() {
+								for input in tx.input.iter() {
+									if input.witness.is_empty() {
+										// We generated a malleable funding transaction, implying we've
+										// just exposed ourselves to funds loss to our counterparty.
+										#[cfg(not(feature = "fuzztarget"))]
+										panic!("Client called ChannelManager::funding_transaction_generated with bogus transaction!");
+									}
 								}
 							}
+							self.funding_tx_confirmation_height = height as u64;
+							self.funding_tx_confirmed_in = Some(*block_hash);
+							self.short_channel_id = match scid_from_parts(height as u64, index_in_block as u64, txo_idx as u64) {
+								Ok(scid) => Some(scid),
+								Err(_) => panic!("Block was bogus - either height was > 16 million, had > 16 million transactions, or had > 65k outputs"),
+							}
 						}
-						self.funding_tx_confirmation_height = height as u64;
-						self.funding_tx_confirmed_in = Some(*block_hash);
-						self.short_channel_id = match scid_from_parts(height as u64, index_in_block as u64, txo_idx as u64) {
-							Ok(scid) => Some(scid),
-							Err(_) => panic!("Block was bogus - either height was > 16 million, had > 16 million transactions, or had > 65k outputs"),
-						}
+					}
+				}
+				for inp in tx.input.iter() {
+					if inp.previous_output == funding_txo.into_bitcoin_outpoint() {
+						log_trace!(logger, "Detected channel-closing tx {} spending {}:{}, closing channel {}", tx.txid(), inp.previous_output.txid, inp.previous_output.vout, log_bytes!(self.channel_id()));
+						return Err(msgs::ErrorMessage {
+							channel_id: self.channel_id(),
+							data: "Commitment or closing transaction was confirmed on chain.".to_owned()
+						});
 					}
 				}
 			}
@@ -3633,35 +3648,6 @@ impl<Signer: Sign> Channel<Signer> {
 		}
 
 		Ok((None, timed_out_htlcs))
-	}
-
-	/// When we receive a new block, we (a) check whether the block contains the funding
-	/// transaction (which would start us counting blocks until we send the funding_signed), and
-	/// (b) check the height of the block against outbound holding cell HTLCs in case we need to
-	/// give up on them prematurely and time them out. Everything else (e.g. commitment
-	/// transaction broadcasts, channel closure detection, HTLC transaction broadcasting, etc) is
-	/// handled by the ChannelMonitor.
-	///
-	/// If we return Err, the channel may have been closed, at which point the standard
-	/// requirements apply - no calls may be made except those explicitly stated to be allowed
-	/// post-shutdown.
-	/// Only returns an ErrorAction of DisconnectPeer, if Err.
-	///
-	/// May return some HTLCs (and their payment_hash) which have timed out and should be failed
-	/// back.
-	pub fn block_connected(&mut self, header: &BlockHeader, txdata: &TransactionData, height: u32) -> Result<(Option<msgs::FundingLocked>, Vec<(HTLCSource, PaymentHash)>), msgs::ErrorMessage> {
-		self.transactions_confirmed(&header.block_hash(), height, txdata)?;
-		self.update_best_block(height, header.time)
-	}
-
-	/// Called by channelmanager based on chain blocks being disconnected.
-	/// Returns true if we need to close the channel now due to funding transaction
-	/// unconfirmation/reorg.
-	pub fn block_disconnected(&mut self, header: &BlockHeader, new_height: u32) -> bool {
-		if self.update_best_block(new_height, header.time).is_err() {
-			return true;
-		}
-		false
 	}
 
 	// Methods to get unprompted messages to send to the remote end (or where we already returned

lightning/src/ln/channelmanager.rs
@@ -3334,7 +3334,8 @@ impl<Signer: Sign, M: Deref, T: Deref, K: Deref, F: Deref, L: Deref> ChannelMana
 			let short_to_id = &mut channel_state.short_to_id;
 			let pending_msg_events = &mut channel_state.pending_msg_events;
 			channel_state.by_id.retain(|_, channel| {
-				let res = channel.block_connected(header, txdata, height);
+				let res = channel.transactions_confirmed(&block_hash, height, txdata, &self.logger)
+					.and_then(|_| channel.update_best_block(height, header.time));
 				if let Ok((chan_res, mut timed_out_pending_htlcs)) = res {
 					for (source, payment_hash) in timed_out_pending_htlcs.drain(..) {
 						let chan_update = self.get_channel_update(&channel).map(|u| u.encode_with_len()).unwrap(); // Cannot add/recv HTLCs before we have a short_id so unwrap is safe
@@ -3360,37 +3361,22 @@ impl<Signer: Sign, M: Deref, T: Deref, K: Deref, F: Deref, L: Deref> ChannelMana
 						short_to_id.insert(channel.get_short_channel_id().unwrap(), channel.channel_id());
 					}
 				} else if let Err(e) = res {
+					if let Some(short_id) = channel.get_short_channel_id() {
+						short_to_id.remove(&short_id);
+					}
+					// It looks like our counterparty went on-chain or funding transaction was
+					// reorged out of the main chain. Close the channel.
+					failed_channels.push(channel.force_shutdown(true));
+					if let Ok(update) = self.get_channel_update(&channel) {
+						pending_msg_events.push(events::MessageSendEvent::BroadcastChannelUpdate {
+							msg: update
+						});
+					}
 					pending_msg_events.push(events::MessageSendEvent::HandleError {
 						node_id: channel.get_counterparty_node_id(),
 						action: msgs::ErrorAction::SendErrorMessage { msg: e },
 					});
 					return false;
-				}
-				if let Some(funding_txo) = channel.get_funding_txo() {
-					for &(_, tx) in txdata.iter() {
-						for inp in tx.input.iter() {
-							if inp.previous_output == funding_txo.into_bitcoin_outpoint() {
-								log_trace!(self.logger, "Detected channel-closing tx {} spending {}:{}, closing channel {}", tx.txid(), inp.previous_output.txid, inp.previous_output.vout, log_bytes!(channel.channel_id()));
-								if let Some(short_id) = channel.get_short_channel_id() {
-									short_to_id.remove(&short_id);
-								}
-								// It looks like our counterparty went on-chain. Close the channel.
-								failed_channels.push(channel.force_shutdown(true));
-								if let Ok(update) = self.get_channel_update(&channel) {
-									pending_msg_events.push(events::MessageSendEvent::BroadcastChannelUpdate {
-										msg: update
-									});
-								}
-								pending_msg_events.push(events::MessageSendEvent::HandleError {
-									node_id: channel.get_counterparty_node_id(),
-									action: msgs::ErrorAction::SendErrorMessage {
-										msg: msgs::ErrorMessage { channel_id: channel.channel_id(), data: "Commitment or closing transaction was confirmed on chain.".to_owned() }
-									},
-								});
-								return false;
-							}
-						}
-					}
 				}
 				true
 			});
@@ -3456,8 +3442,8 @@ impl<Signer: Sign, M: Deref, T: Deref, K: Deref, F: Deref, L: Deref> ChannelMana
 			let channel_state = &mut *channel_lock;
 			let short_to_id = &mut channel_state.short_to_id;
 			let pending_msg_events = &mut channel_state.pending_msg_events;
-			channel_state.by_id.retain(|channel_id,  v| {
-				if v.block_disconnected(header, new_height) {
+			channel_state.by_id.retain(|_,  v| {
+				if let Err(err_msg) = v.update_best_block(new_height, header.time) {
 					if let Some(short_id) = v.get_short_channel_id() {
 						short_to_id.remove(&short_id);
 					}
@@ -3469,9 +3455,7 @@ impl<Signer: Sign, M: Deref, T: Deref, K: Deref, F: Deref, L: Deref> ChannelMana
 					}
 					pending_msg_events.push(events::MessageSendEvent::HandleError {
 						node_id: v.get_counterparty_node_id(),
-						action: msgs::ErrorAction::SendErrorMessage {
-							msg: msgs::ErrorMessage { channel_id: *channel_id, data: "Funding transaction was un-confirmed.".to_owned() }
-						},
+						action: msgs::ErrorAction::SendErrorMessage { msg: err_msg },
 					});
 					false
 				} else {

liveseed/rust-concurrency

commit 7524bf627112994eb0646ce9607cd6ce05c945b9

Fix (and test) panic when our counterparty uses a bogus funding tx
During the block API refactor, we started calling
Channel::force_shutdown when a channel is closed due to a bogus
funding tx. However, we still set the channel's state to Shutdown
prior to doing so, leading to an assertion in force_shutdown (that
the channel is not already closed).

This removes the state-set call and adds a (long-overdue) test for
this case.

Fixes: 60b962a18ebcf494340ddc001870f8160c625968

lightning/src/ln/channel.rs
@@ -3574,7 +3574,6 @@ impl<Signer: Sign> Channel<Signer> {
 								#[cfg(not(feature = "fuzztarget"))]
 								panic!("Client called ChannelManager::funding_transaction_generated with bogus transaction!");
 							}
-							self.channel_state = ChannelState::ShutdownComplete as u32;
 							self.update_time_counter += 1;
 							return Err(msgs::ErrorMessage {
 								channel_id: self.channel_id(),

lightning/src/ln/channelmanager.rs
@@ -1563,61 +1563,14 @@ impl<Signer: Sign, M: Deref, T: Deref, K: Deref, F: Deref, L: Deref> ChannelMana
 		}
 	}
 
-	/// Call this upon creation of a funding transaction for the given channel.
-	///
-	/// Returns an [`APIError::APIMisuseError`] if the funding_transaction spent non-SegWit outputs
-	/// or if no output was found which matches the parameters in [`Event::FundingGenerationReady`].
-	///
-	/// Panics if a funding transaction has already been provided for this channel.
-	///
-	/// May panic if the output found in the funding transaction is duplicative with some other
-	/// channel (note that this should be trivially prevented by using unique funding transaction
-	/// keys per-channel).
-	///
-	/// Do NOT broadcast the funding transaction yourself. When we have safely received our
-	/// counterparty's signature the funding transaction will automatically be broadcast via the
-	/// [`BroadcasterInterface`] provided when this `ChannelManager` was constructed.
-	///
-	/// Note that this includes RBF or similar transaction replacement strategies - lightning does
-	/// not currently support replacing a funding transaction on an existing channel. Instead,
-	/// create a new channel with a conflicting funding transaction.
-	pub fn funding_transaction_generated(&self, temporary_channel_id: &[u8; 32], funding_transaction: Transaction) -> Result<(), APIError> {
-		let _persistence_guard = PersistenceNotifierGuard::new(&self.total_consistency_lock, &self.persistence_notifier);
-
-		for inp in funding_transaction.input.iter() {
-			if inp.witness.is_empty() {
-				return Err(APIError::APIMisuseError {
-					err: "Funding transaction must be fully signed and spend Segwit outputs".to_owned()
-				});
-			}
-		}
-
+	/// Handles the generation of a funding transaction, optionally (for tests) with a function
+	/// which checks the correctness of the funding transaction given the associated channel.
+	fn funding_transaction_generated_intern<FundingOutput: Fn(&Channel<Signer>, &Transaction) -> Result<OutPoint, APIError>>
+			(&self, temporary_channel_id: &[u8; 32], funding_transaction: Transaction, find_funding_output: FundingOutput) -> Result<(), APIError> {
 		let (chan, msg) = {
 			let (res, chan) = match self.channel_state.lock().unwrap().by_id.remove(temporary_channel_id) {
 				Some(mut chan) => {
-					let mut output_index = None;
-					let expected_spk = chan.get_funding_redeemscript().to_v0_p2wsh();
-					for (idx, outp) in funding_transaction.output.iter().enumerate() {
-						if outp.script_pubkey == expected_spk && outp.value == chan.get_value_satoshis() {
-							if output_index.is_some() {
-								return Err(APIError::APIMisuseError {
-									err: "Multiple outputs matched the expected script and value".to_owned()
-								});
-							}
-							if idx > u16::max_value() as usize {
-								return Err(APIError::APIMisuseError {
-									err: "Transaction had more than 2^16 outputs, which is not supported".to_owned()
-								});
-							}
-							output_index = Some(idx as u16);
-						}
-					}
-					if output_index.is_none() {
-						return Err(APIError::APIMisuseError {
-							err: "No output matched the script_pubkey and value in the FundingGenerationReady event".to_owned()
-						});
-					}
-					let funding_txo = OutPoint { txid: funding_transaction.txid(), index: output_index.unwrap() };
+					let funding_txo = find_funding_output(&chan, &funding_transaction)?;
 
 					(chan.get_outbound_funding_created(funding_transaction, funding_txo, &self.logger)
 						.map_err(|e| if let ChannelError::Close(msg) = e {
@@ -1651,6 +1604,68 @@ impl<Signer: Sign, M: Deref, T: Deref, K: Deref, F: Deref, L: Deref> ChannelMana
 			}
 		}
 		Ok(())
+	}
+
+	#[cfg(test)]
+	pub(crate) fn funding_transaction_generated_unchecked(&self, temporary_channel_id: &[u8; 32], funding_transaction: Transaction, output_index: u16) -> Result<(), APIError> {
+		self.funding_transaction_generated_intern(temporary_channel_id, funding_transaction, |_, tx| {
+			Ok(OutPoint { txid: tx.txid(), index: output_index })
+		})
+	}
+
+	/// Call this upon creation of a funding transaction for the given channel.
+	///
+	/// Returns an [`APIError::APIMisuseError`] if the funding_transaction spent non-SegWit outputs
+	/// or if no output was found which matches the parameters in [`Event::FundingGenerationReady`].
+	///
+	/// Panics if a funding transaction has already been provided for this channel.
+	///
+	/// May panic if the output found in the funding transaction is duplicative with some other
+	/// channel (note that this should be trivially prevented by using unique funding transaction
+	/// keys per-channel).
+	///
+	/// Do NOT broadcast the funding transaction yourself. When we have safely received our
+	/// counterparty's signature the funding transaction will automatically be broadcast via the
+	/// [`BroadcasterInterface`] provided when this `ChannelManager` was constructed.
+	///
+	/// Note that this includes RBF or similar transaction replacement strategies - lightning does
+	/// not currently support replacing a funding transaction on an existing channel. Instead,
+	/// create a new channel with a conflicting funding transaction.
+	pub fn funding_transaction_generated(&self, temporary_channel_id: &[u8; 32], funding_transaction: Transaction) -> Result<(), APIError> {
+		let _persistence_guard = PersistenceNotifierGuard::new(&self.total_consistency_lock, &self.persistence_notifier);
+
+		for inp in funding_transaction.input.iter() {
+			if inp.witness.is_empty() {
+				return Err(APIError::APIMisuseError {
+					err: "Funding transaction must be fully signed and spend Segwit outputs".to_owned()
+				});
+			}
+		}
+		self.funding_transaction_generated_intern(temporary_channel_id, funding_transaction, |chan, tx| {
+			let mut output_index = None;
+			let expected_spk = chan.get_funding_redeemscript().to_v0_p2wsh();
+			for (idx, outp) in tx.output.iter().enumerate() {
+				if outp.script_pubkey == expected_spk && outp.value == chan.get_value_satoshis() {
+					if output_index.is_some() {
+						return Err(APIError::APIMisuseError {
+							err: "Multiple outputs matched the expected script and value".to_owned()
+						});
+					}
+					if idx > u16::max_value() as usize {
+						return Err(APIError::APIMisuseError {
+							err: "Transaction had more than 2^16 outputs, which is not supported".to_owned()
+						});
+					}
+					output_index = Some(idx as u16);
+				}
+			}
+			if output_index.is_none() {
+				return Err(APIError::APIMisuseError {
+					err: "No output matched the script_pubkey and value in the FundingGenerationReady event".to_owned()
+				});
+			}
+			Ok(OutPoint { txid: tx.txid(), index: output_index.unwrap() })
+		})
 	}
 
 	fn get_announcement_sigs(&self, chan: &Channel<Signer>) -> Option<msgs::AnnouncementSignatures> {

lightning/src/ln/functional_tests.rs
@@ -8825,3 +8825,53 @@ fn test_error_chans_closed() {
 	assert_eq!(nodes[0].node.list_usable_channels().len(), 1);
 	assert!(nodes[0].node.list_usable_channels()[0].channel_id == chan_3.2);
 }
+
+#[test]
+fn test_invalid_funding_tx() {
+	// Test that we properly handle invalid funding transactions sent to us from a peer.
+	//
+	// Previously, all other major lightning implementations had failed to properly sanitize
+	// funding transactions from their counterparties, leading to a multi-implementation critical
+	// security vulnerability (though we always sanitized properly, we've previously had
+	// un-released crashes in the sanitization process).
+	let chanmon_cfgs = create_chanmon_cfgs(2);
+	let node_cfgs = create_node_cfgs(2, &chanmon_cfgs);
+	let node_chanmgrs = create_node_chanmgrs(2, &node_cfgs, &[None, None]);
+	let nodes = create_network(2, &node_cfgs, &node_chanmgrs);
+
+	nodes[0].node.create_channel(nodes[1].node.get_our_node_id(), 100_000, 10_000, 42, None).unwrap();
+	nodes[1].node.handle_open_channel(&nodes[0].node.get_our_node_id(), InitFeatures::known(), &get_event_msg!(nodes[0], MessageSendEvent::SendOpenChannel, nodes[1].node.get_our_node_id()));
+	nodes[0].node.handle_accept_channel(&nodes[1].node.get_our_node_id(), InitFeatures::known(), &get_event_msg!(nodes[1], MessageSendEvent::SendAcceptChannel, nodes[0].node.get_our_node_id()));
+
+	let (temporary_channel_id, mut tx, _) = create_funding_transaction(&nodes[0], 100_000, 42);
+	for output in tx.output.iter_mut() {
+		// Make the confirmed funding transaction have a bogus script_pubkey
+		output.script_pubkey = bitcoin::Script::new();
+	}
+
+	nodes[0].node.funding_transaction_generated_unchecked(&temporary_channel_id, tx.clone(), 0).unwrap();
+	nodes[1].node.handle_funding_created(&nodes[0].node.get_our_node_id(), &get_event_msg!(nodes[0], MessageSendEvent::SendFundingCreated, nodes[1].node.get_our_node_id()));
+	check_added_monitors!(nodes[1], 1);
+
+	nodes[0].node.handle_funding_signed(&nodes[1].node.get_our_node_id(), &get_event_msg!(nodes[1], MessageSendEvent::SendFundingSigned, nodes[0].node.get_our_node_id()));
+	check_added_monitors!(nodes[0], 1);
+
+	let events_1 = nodes[0].node.get_and_clear_pending_events();
+	assert_eq!(events_1.len(), 0);
+
+	assert_eq!(nodes[0].tx_broadcaster.txn_broadcasted.lock().unwrap().len(), 1);
+	assert_eq!(nodes[0].tx_broadcaster.txn_broadcasted.lock().unwrap()[0], tx);
+	nodes[0].tx_broadcaster.txn_broadcasted.lock().unwrap().clear();
+
+	confirm_transaction_at(&nodes[1], &tx, 1);
+	check_added_monitors!(nodes[1], 1);
+	let events_2 = nodes[1].node.get_and_clear_pending_msg_events();
+	assert_eq!(events_2.len(), 1);
+	if let MessageSendEvent::HandleError { node_id, action } = &events_2[0] {
+		assert_eq!(*node_id, nodes[0].node.get_our_node_id());
+		if let msgs::ErrorAction::SendErrorMessage { msg } = action {
+			assert_eq!(msg.data, "funding tx had wrong script/value or output index");
+		} else { panic!(); }
+	} else { panic!(); }
+	assert_eq!(nodes[1].node.list_channels().len(), 0);
+}